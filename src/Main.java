1692f23aa2ac851a17bfb5b59a5cc86dfca06d6d
Serg <ksyssau@ya.ru>
2023-05-07 21:19:51 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int[] inArray = {1, 2, 3};
+        int[] outArray = new int[inArray.length];
+        for (int i = 0; i < inArray.length; i++) {
+            outArray[i] = inArray[inArray.length - i - 1];
+            System.out.println(outArray[i]);
+        }
+    }
+}
